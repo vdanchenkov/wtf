fb08a92f6b8bd76c7166f8f5daffeae0955e755d
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-09-21 02:56:31 +0400

    Implement query

--- src/lib/query.js
+++ src/lib/query.js
@@ -1,6 +1,13 @@
+import argumentVariants from './argumentVariants'
+
 /**
 * Returns query
 */
 export default (definition) => {
-  
+  const query = []
+  const wtf = (...args) => (
+    { isEqual: (expectation) => query.push({ argumentVariants: argumentVariants(...args), expectation }) }
+  )
+  definition(wtf)
+  return query
 }
--- test/query.js
+++ test/query.js
@@ -2,9 +2,33 @@ import test from "tape"
 
 import query from '../src/lib/query'
 
-test.skip('simple query', (t) => {
+test('simple query', (t) => {
   const result = query(wtf => wtf(1).isEqual(1))
-  t.equal(result.length, 1)
-  t.deepEqual(result[0], { combinations: [], result: 1 }) 
+  t.deepEqual(result, [ 
+    { argumentVariants: [ 
+      { argumentLabels: [ '1' ], argumentValues: [ 1 ] } 
+    ], expectation: 1 } 
+  ]) 
+  t.end()
+})
+
+test('two queries', (t) => {
+  const result = query(wtf => {
+    wtf(2, 2).isEqual(4)
+    wtf(3, 5).isEqual(8)
+  })
+  t.deepEqual(result, [ 
+    { 
+      argumentVariants: [ 
+        { argumentLabels: [ '2', '2' ], argumentValues: [ 2, 2 ] }, 
+        { argumentLabels: [ '2', '2' ], argumentValues: [ 2, 2 ] } 
+      ], expectation: 4 
+    },
+    { 
+      argumentVariants: [
+        { argumentLabels: [ '3', '5' ], argumentValues: [ 3, 5 ] }, 
+        { argumentLabels: [ '5', '3' ], argumentValues: [ 5, 3 ] } 
+      ], expectation: 8 } 
+  ]) 
   t.end()
 })
