ace98146826bf556678c4e47a84758636d94c665
Vladimir Danchenkov <vladimir.danchenkov@gmail.com>
2016-10-04 22:23:36 +0400

    fix test

--- test/destruct.js
+++ test/destruct.js
@@ -1,17 +1,20 @@
 import { destructFunction } from '../src/lib';
 
 test('thing', t => {
+  t.plan(2)
   let i = 0;
 
   const fn = destructFunction(2)(() => {
     i++;
   });
 
-  for (let i = 0; i < 100; i++) {
-    fn();
+  try {
+    for (let i = 0; i < 100; i++) {
+      fn();
+    }
+  } catch  (e) {
+    t.pass('throwed')
   }
 
   t.equal(i, 2);
-
-  t.end();
 });
